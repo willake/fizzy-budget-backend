294047a45887e27e885cc967f6f5893749391454
Hui <huienlin.game@gmail.com>
2024-09-23 22:02:15 +0200

    add: add shared entity project for sharing some code for entity

---
+++ shared-entity/src/test/java/com/huiun/fizzybudget/shared_entity/SharedEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huiun.fizzybudget.shared_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
