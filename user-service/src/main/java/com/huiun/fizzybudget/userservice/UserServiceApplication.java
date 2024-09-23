cb81adb233932855199df81149579ed2c911a3ca
Hui <huienlin.game@gmail.com>
2024-09-24 00:24:32 +0200

    update: fixed sharedentities was not scanned issue

--- user-service/src/main/java/com/huiun/fizzybudget/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/huiun/fizzybudget/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.huiun.fizzybudget.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.huiun.fizzybudget.sharedentities") // Adjust the package to your shared entities package
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
